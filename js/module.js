5725eedbb9d8e04d46b9cc7e58bd555001e8eee3
Contillo <61m37.17@gmail.com>
2019-03-09 06:32:57 +0800

    about fix

--- js/module.js
+++ js/module.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-		$('#about').load('contents/webfw-skills.html');
+		$('#about').load('contents/about.html');
 		$('#webdev-skills').load('contents/webdev-skills.html');
 		$('#webframeworks-skills').load('contents/webfw-skills.html');
 });
