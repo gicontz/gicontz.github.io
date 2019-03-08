bcf59dc0c6f9f08805e5e6f25d03d6aca29551da
Contillo <61m37.17@gmail.com>
2019-03-09 06:29:19 +0800

    Fix unload contents

--- js/module.js
+++ js/module.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+		$('#about').load('contents/webfw-skills.html');
 		$('#webdev-skills').load('contents/webdev-skills.html');
 		$('#webframeworks-skills').load('contents/webfw-skills.html');
 });
