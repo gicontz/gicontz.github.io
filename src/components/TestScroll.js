3eceb3f38c0c77489bec25ea7f410dea68683f31
sensei_gim <gimelcontz@gmail.com>
2019-10-19 11:10:51 +0800

    Add themes

---
+++ src/components/TestScroll.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+function TestScroll() {
+  return (
+    <div>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    <br/>
+    <p>Hello!</p>
+    </div>
+  );
+}
+
+export default TestScroll;
--- src/components/hero-banner/HeroBanner.js
+++ src/components/hero-banner/HeroBanner.js
@@ -1,13 +1,49 @@
 import React from 'react';
-import {__screenHeight} from '../../models/Dimensions'
+import { __screenHeight } from '../../models/Dimensions'
 import './HeroBanner.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styled from "styled-components";
 import Text from "../Text";
 
+/***** COMPONENTS  *****/
+import Profile from '../profile/Profile'
+import TestScroll from '../TestScroll'
+
+const items = [{
+    key: '1',
+    text: '1'
+},
+{
+    key: '2',
+    text: '2'
+},
+{
+    key: '3',
+    text: '3'
+}];
+
 const bannerHeight = {
   height: __screenHeight()
 };
+
+const sections = [
+  {  id: 'profile',
+     url: "profile-sect" }, 
+  { id: 'experience',
+    url: "exp-sect" }, 
+  { id: 'works',
+    url: "work-sect" },
+  { id: 'stacks',
+    url: "stacks-sect" },
+  { id: 'contact',
+    url: "contact-sect" }
+];
+
+const __menus = sections.map((section) =>
+  <li key={section.id}>
+    <a href={'#' + section.url}>{section.id.toUpperCase()}</a>
+  </li>
+);
 
 const HeroBanner = props => {
   var theme = props.apptheme;
@@ -15,25 +51,33 @@ const HeroBanner = props => {
   return (
       <header className="App-header" style={bannerHeight}>
         <div className="row">
-          <div className="social-icons col-md-3">
-            <ul>
-              <li><a href="#"><FontAwesomeIcon icon={['fab', 'facebook']} size="2x"/></a></li>
-              <li><a href="#"><FontAwesomeIcon icon={['fab', 'twitter']} size="2x"/></a></li>
-              <li><a href="#"><FontAwesomeIcon icon={['fab', 'github']} size="2x"/></a></li>
-              <li><a href="#"><FontAwesomeIcon icon={['fab', 'instagram']} size="2x"/></a></li>              
-            </ul>
+          <div className="main-content col-md-7">
+          <Profile />
+          <TestScroll/>
+            <div className="social-icons">
+              <ul>
+                <li><a href="#"><FontAwesomeIcon icon={['fab', 'facebook']} size="2x"/></a></li>
+                <li><a href="#"><FontAwesomeIcon icon={['fab', 'twitter']} size="2x"/></a></li>
+                <li><a href="#"><FontAwesomeIcon icon={['fab', 'github']} size="2x"/></a></li>
+                <li><a href="#"><FontAwesomeIcon icon={['fab', 'instagram']} size="2x"/></a></li>              
+              </ul>
+            </div>
           </div>
-          <div className="side-menu col-md-5 offset-md-4" style={bannerHeight}>
+          <div className="side-menu col-md-5" style={bannerHeight}>
             <div className="menu-nav text-left">
-                <a href="#">
+                <a href="#" className="mobile-menu">
                   <FontAwesomeIcon icon="bars" size="sm" className="menu-bar-icon" /><span>MENU</span>
                 </a>
+
+                <ul className="menu-list sm-scr">
+                  { __menus }
+                </ul>
             </div>
               <div className="def-section">
                 <dl className="name-def">
                   <rt>/jim-el/</rt>
                   <dt><h1>gimel</h1></dt>
-                  <Text fontSize={[13,20]} color="blue">third alphabet character <br/>
+                  <Text fontSize={[13,20]} color="#565656">third alphabet character <br/>
                       in Hebrew and the first <br/>
                       name of a game changer</Text>
                 </dl>
