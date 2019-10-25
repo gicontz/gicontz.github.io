ca4806f15a006c9701e776714b8ef06a8bcb0976
sensei_gim <gimelcontz@gmail.com>
2019-10-25 15:20:06 +0800

    Conversite to react_dev

--- js/module.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(() => {
-		$('#about').load('contents/about.html');
-		$('#webdev-skills').load('contents/webdev-skills.html');
-		$('#webframeworks-skills').load('contents/webfw-skills.html');
-});
---
+++ precache-manifest.7c6b3aad9757bf09922ec23610d5d833.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f211fd171e631e41d7f6e2c6363a47fd",
+    "url": "/gicontz.github.io/index.html"
+  },
+  {
+    "revision": "a78cbcc2268eb439a57e",
+    "url": "/gicontz.github.io/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5a708daf91e7a43831f8",
+    "url": "/gicontz.github.io/static/css/main.f30abcd0.chunk.css"
+  },
+  {
+    "revision": "a78cbcc2268eb439a57e",
+    "url": "/gicontz.github.io/static/js/2.24a4a516.chunk.js"
+  },
+  {
+    "revision": "5a708daf91e7a43831f8",
+    "url": "/gicontz.github.io/static/js/main.29f708ad.chunk.js"
+  },
+  {
+    "revision": "5d191829e070b09858e6",
+    "url": "/gicontz.github.io/static/js/runtime-main.2e33f7a5.js"
+  },
+  {
+    "revision": "9ccb885c9cf8e503d557f15e0b2cbf24",
+    "url": "/gicontz.github.io/static/media/Merriweather-Bold.9ccb885c.ttf"
+  },
+  {
+    "revision": "c97a9fc29652bb4afcdac68020e5d0f7",
+    "url": "/gicontz.github.io/static/media/Merriweather-Regular.c97a9fc2.ttf"
+  },
+  {
+    "revision": "3ada7a9482cb9a123ad501e45053adb3",
+    "url": "/gicontz.github.io/static/media/Oxygen-Bold.3ada7a94.ttf"
+  },
+  {
+    "revision": "dfefd7db6a479b1161e9f0f9cb0abb8d",
+    "url": "/gicontz.github.io/static/media/Oxygen-Light.dfefd7db.ttf"
+  },
+  {
+    "revision": "61d9daf063ba38f2d05f8adb7267e6fd",
+    "url": "/gicontz.github.io/static/media/Oxygen-Regular.61d9daf0.ttf"
+  },
+  {
+    "revision": "244c813f374011e3df1f90b08de7976e",
+    "url": "/gicontz.github.io/static/media/banner.244c813f.jpg"
+  }
+]);
