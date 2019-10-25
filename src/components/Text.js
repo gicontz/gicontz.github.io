77643c23618c8c427eb34334a1c8926b1451a7cc
sensei_gim <gimelcontz@gmail.com>
2019-10-25 14:40:29 +0800

    Integrate FullPage.js

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 import { layout , color } from "styled-system";
+import { typography } from "styled-system";
+import fluid from "fluid-system";
 
-const Text = styled.p`
-  ${layout}
-  ${color}
-`;
-
+const Text = styled("p")(
+  fluid(typography) 
+)
 export default Text;
